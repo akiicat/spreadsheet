025eee32f4506bec32e38a3ad964b108e1f1c4bd
Akiicat <aaaa1379@gmail.com>
2017-03-20 17:22:05 +0800

    create spreadsheet index

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
+  get 'spreadsheet/index'
+  root 'spreadsheet#index'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  mount ActionCable.server => '/cable'
 end
