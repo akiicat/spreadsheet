6ae7ec857b6e9878f562e3519adca662f404fff0
Akiicat <aaaa1379@gmail.com>
2017-03-21 02:05:09 +0800

    Streaming field selections as colorful cells

--- app/channels/active_users_channel.rb
+++ app/channels/active_users_channel.rb
@@ -3,10 +3,18 @@ class ActiveUsersChannel < ApplicationCable::Channel
 
   def subscribed
     @user = User.create
+    transmit current_user: @user
     stream_from User.all, include_initial: true
   end
 
   def unsubscribed
     @user.destroy
+  end
+
+  def select_cells(message)
+    @user.update! selected_cells: message['selected_cells']
+    #message.delete :action
+    #@user.selected_cells = message
+    #@user.save
   end
 end
